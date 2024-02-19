d98841fdef2dcdb467b123b62e80108d7c0b9dc7
Ghassan-Awdali <awdalig@gmail.com>
2024-02-18 22:50:27 -0400

    finished styling and designing home, working on social links of the website modern design techniques with hover animations, and react icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import Socials from "./components/Socials";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
      <NavBar/>
      <Home/>
+     <Socials/>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
 
 
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4
+    <div className='flex justify-between items-center w-full h-18 px-4
      text-white bg-black fixed'>
         <div>
         <h1 className = 'text-5xl font-signature ml-2'>Ghassan</h1>
