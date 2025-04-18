58cf48970e2b109e406fc8df5325143a705d638c
G ARUTO <awdalig@gmail.com>
2025-04-18 11:20:16 -0300

    responsive website

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,52 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div
-    name="contact"
-    className="sm:pt-32 w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
-  >
-    <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-      <div className="pb-8">
-        <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-          Contact
-        </p>
-        <p className="py-6 ">Submit the form below to get in touch with me</p>
-      </div>
+      name="contact"
+      className="w-full min-h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white pt-20 md:pt-0"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contact
+          </p>
+          <p className="py-6 ">Submit the form below to get in touch with me</p>
+        </div>
 
-      <div className=" sm:flex sm:justify-center sm:items-center flex justify-center items-center">
-        <form
-          action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
-          method="POST"
-          className=" flex flex-col w-full md:w-1/2"
-        >
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-            className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-          />
-          <input
-            type="text"
-            name="email"
-            placeholder="Enter your email"
-            className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-          />
-          <textarea
-            name="message"
-            placeholder="Enter your message"
-            rows="10"
-            className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-          ></textarea>
+        <div className=" sm:flex sm:justify-center sm:items-center flex justify-center items-center">
+          <form
+            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            method="POST"
+            className=" flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter your email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Enter your message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
 
-          <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 sm:px-5 sm:py-2 sm:mx-auto sm:flex sm:items-center">
-            Connect!
-          </button>
-        </form>
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 sm:px-5 sm:py-2 sm:mx-auto sm:flex sm:items-center">
+              Connect!
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
